30487db55de77e3bcb8080a7529928d7a0534ca2
Aloysius <dr.aloysius@gmail.com>
2020-12-20 07:46:34 +0000

    Turn into reusable function

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-/*
- * Iterating a list of asynchronous tasks sequentially via callbacks
- */
+const runSequential = require('./runSequential.js')
 
 const tasks = [
   // 1. Each async task should accept a callback
@@ -9,20 +7,12 @@ const tasks = [
   (cb) => setTimeout(cb, 1000),
 ];
 
-function runTask (index) {
-  // Stop when we've run through to the end of the task list
-  if (index === tasks.length) {
-    return 'finshed'
+runSequential(
+  tasks,
+  data => {
+    console.log(`Ran task ${data.taskIndex}`)
+  },
+  () => {
+    console.log(`All done :)`)
   }
-  
-  const asyncTask = tasks[index]
-  // 2. Run the async task
-  asyncTask(() => {
-    // When this callback is run it means the previous task was complete, so it starts the next task in the list.
-    console.log(`Ran task ${index}`)
-    runTask(index + 1)
-  })
-}
-
-// Start the execution flow
-runTask(0)
+)
---
+++ runSequential.js
@@ -0,0 +1,26 @@
+/**
+ * Runs each function in <collection> sequentially using callback execution flow.
+ * 
+ * @param {Array} collection A list of functions to run
+ * @param {function} callback A function to run after each function in <collection>
+ * @param {function} finalCallback The function to run after the last function in <collection> has run
+ */
+module.exports = function runSequential(collection, callback, finalCallback) {
+
+  let index = 0;
+
+  function runNextTask(index) {
+    if (index === collection.length) {
+      return finalCallback();
+    }
+
+    const task = collection[index]
+    
+    task(() => {
+      callback({taskIndex: index})
+      runNextTask(++index)
+    })
+  }
+
+  runNextTask(index)
+}
